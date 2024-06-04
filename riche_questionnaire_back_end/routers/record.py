a53046050ee7bac0bf3d0f8b3d0da80c802d959a
Ilya Barinov <ilya.bv.2002@bk.ru>
2024-06-04 18:03:13 +0300

    add comment

--- riche_questionnaire_back_end/routers/record.py
+++ riche_questionnaire_back_end/routers/record.py
@@ -19,6 +19,15 @@ records_router = APIRouter()
 
 @records_router.post("/create-record")
 async def create_record(data_in: DataIn, db: Session = Depends(get_db)):
+    """_summary_
+
+    Args:
+        data_in (DataIn): _description_
+        db (Session, optional): _description_. Defaults to Depends(get_db).
+
+    Returns:
+        _type_: _description_
+    """
     record_id = str(uuid4())
     database[record_id] = data_in.data
     return {"uuid": record_id}
@@ -26,6 +35,17 @@ async def create_record(data_in: DataIn, db: Session = Depends(get_db)):
 
 @records_router.get("/read-record")
 async def read_record(record_id: str):
+    """_summary_
+
+    Args:
+        record_id (str): _description_
+
+    Raises:
+        HTTPException: _description_
+
+    Returns:
+        _type_: _description_
+    """
     if record_id not in database:
         raise HTTPException(status_code=404, detail="Запись не найдена")
     return {"data": database[record_id]}
