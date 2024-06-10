1c4d3876855c1f311d4d0821aa28e184d1c5f54a
Ilya Barinov <ilya.bv.2002@bk.ru>
2024-06-10 14:51:08 +0300

    clear and code reviev

--- riche_questionnaire_back_end/routers/record.py
+++ riche_questionnaire_back_end/routers/record.py
@@ -1,9 +1,9 @@
-from typing import Dict, Optional, Union
 from fastapi import APIRouter, Depends, HTTPException
 from fastapi.responses import JSONResponse
+from typing import Dict, Optional, Union
+from sqlalchemy.orm import Session
 from pydantic import BaseModel
 from uuid import uuid4
-from sqlalchemy.orm import Session
 
 from riche_questionnaire_back_end.db import get_db
 from riche_questionnaire_back_end.models.survey_models import (
@@ -12,88 +12,37 @@ from riche_questionnaire_back_end.models.survey_models import (
     Question,
 )
 
-# Пример простой БД в памяти для хранения данных
-database = {}
-
-
-# Модель для передаваемых данных
-class DataIn(BaseModel):
-    data: str
 
 
 records_router = APIRouter()
 
 
-@records_router.post("/create-record")
-async def create_record(data_in: DataIn, db: Session = Depends(get_db)):
-    """_summary_
-
-    Args:
-        data_in (DataIn): _description_
-        db (Session, optional): _description_. Defaults to Depends(get_db).
-
-    Returns:
-        _type_: _description_
-    """
-    record_id = str(uuid4())
-    database[record_id] = data_in.data
-    return {"uuid": record_id}
-
-
-@records_router.get("/read-record")
-async def read_record(record_id: str) -> DataIn:
-    """_summary_
-
-    Args:
-        record_id (str): _description_
-
-    Raises:
-        HTTPException: _description_
-
-    Returns:
-        _type_: _description_
-    """
-    if record_id not in database:
-        raise HTTPException(status_code=404, detail="Запись не найдена")
-    return {"data": database[record_id]}
-
-
-class Question(BaseModel):
+class QuestionValid(BaseModel):
     question: str
     answers: Dict[int, str]
 
 
-class Survey(BaseModel):
+class SurveyValid(BaseModel):
     name: str
     id: Optional[Union[int, str]] = None
-    data: Dict[int, Question]
+    data: Dict[int, QuestionValid]
 
 
 data = {
     "name": "Тестовый опрос",
-    "id": "BigInteger | None",
+    "id": None,
     "data": {
-        1: {
+        "1": {
             "question": "Назовите ваш имя",
-            "ansvers": {
-                1: "Илья",
-                2: "Вадим",
-                3: "Миша",
-            },
+            "answers": {"1": "Илья", "2": "Вадим", "3": "Миша"},
         },
-        2: {
-            "question": "Назовите ваш пол",
-            "ansvers": {
-                1: "М",
-                2: "Ж",
-            },
-        },
+        "2": {"question": "Назовите ваш пол", "answers": {"1": "М", "2": "Ж"}},
     },
 }
 
 
 @records_router.post("/create-survey")
-async def create_survey(survey: Survey, db: Session = Depends(get_db)):
+async def create_survey(survey: SurveyValid = data, db: Session = Depends(get_db)):
     """Функция создания нового опроса
 
     Args:
@@ -107,20 +56,18 @@ async def create_survey(survey: Survey, db: Session = Depends(get_db)):
     """
 
     if survey.id:
-        # Здесь логика изменения текущих данных у опросника
         return JSONResponse(status_code=200, content={"change": True})
-    survey_exists = (
-        db.query(CustomerAction).filter(CustomerAction.name == survey.name).first()
-    )
-    if survey_exists:
-        raise HTTPException(
-            status_code=400, detail="Survey with this name already exists"
-        )
+    # survey_exists = (
+    #     db.query(CustomerAction).filter(CustomerAction.name == survey.name).first()
+    # )
+    # if survey_exists:
+    #     raise HTTPException(
+    #         status_code=400, detail="Survey with this name already exists"
+    #     )
 
     new_survey = CustomerAction(name=survey.name)
     db.add(new_survey)
     db.commit()
-    db.refresh(new_survey)
 
     for question_order, question_data in survey.data.items():
         new_question = Question(
@@ -143,70 +90,31 @@ async def create_survey(survey: Survey, db: Session = Depends(get_db)):
 
 
 @records_router.get("/get-survey")
-async def create_survey(survey_id: int, db: Session = Depends(get_db)):
-    """Функция создания нового опроса
-
-    Args:
-        survey (Survey): _description_
-
-    Raises:
-        HTTPException: _description_
-
-    Returns:
-        int: id_new_survey
-    """
-
-    survey_id = db.query(CustomerAction).get(survey_id)
-    if not (survey_id):
-        return JSONResponse(
-            status_code=200, content={"status": False, "message": "Опросник не найде"}
-        )
-    return JSONResponse(status_code=200, content={"status": True, "data": data})
-
-
-@records_router.get("/customer_actions_with_question_and_answer")
-async def get_customer_actions_with_question_and_answer_route(
-    db: Session = Depends(get_db),
-):
-
-    result = (
-        db.query(CustomerAction)
-        .join(Question, CustomerAction.id == Question.customer_id)
-        .all()
-    )
-
-    return JSONResponse(status_code=200, content={"customer_id": result.customer_id})
-
-
-@records_router.get("/create-survey-from-db")
-async def create_survey_from_db(survey_id: int, db: Session = Depends(get_db)):
+async def get_survey(survey_id: int, db: Session = Depends(get_db)):
     """Функция получения данных из базы и создания опросника по структуре"""
 
     survey_data = db.query(CustomerAction).get(survey_id)
     if not survey_data:
         raise HTTPException(status_code=404, detail="Опросник не найден в базе данных")
 
-    survey_name = survey_data.name
-    survey_questions = db.query(Question).filter(Question.customer_id == survey_id).all()
+    survey_questions = (
+        db.query(Question).filter(Question.customer_id == survey_id).all()
+    )
 
-    survey_data_structure = {
-        "name": survey_name,
-        "id": survey_id,
-        "data": {}
-    }
+    survey_data_structure = {"name": survey_data.name, "id": survey_id, "data": {}}
 
     for question in survey_questions:
-        answers = db.query(AnswerOption).filter(AnswerOption.question_id == question.id).all()
+        answers = (
+            db.query(AnswerOption).filter(AnswerOption.question_id == question.id).all()
+        )
         answers_dict = {answer.ordering: answer.text for answer in answers}
 
         survey_data_structure["data"][question.ordering] = {
             "question": question.text,
-            "answers": answers_dict
+            "answers": answers_dict,
         }
 
-    # Логика проверки данных и обновления
     if survey_data_structure["id"] is not None:
-
         pass
 
     return JSONResponse(status_code=200, content=survey_data_structure)
